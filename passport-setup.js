bce0df7289cb124f03398cb7f55bdb1aca21ddd7
kiettran <kiet.tran@s3corp.com.vn>
2020-09-03 16:07:00 +0700

    fix cookie recure issue

--- passport-setup.js
+++ passport-setup.js
@@ -1,47 +1,59 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth2").Strategy;
-const User = require("./models/user");
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth2').Strategy
+const User = require('./models/user')
+
+const NODE_ENV = process.env.NODE_ENV
+
+const CLIENT_ID =
+  NODE_ENV === 'development'
+    ? process.env.GG_CLIENT_ID_DEV
+    : process.env.GG_CLIENT_ID_PROD
+const CLIENT_SECRET =
+  NODE_ENV === 'development'
+    ? process.env.GG_CLIENT_SECRET_DEV
+    : process.env.GG_CLIENT_SECRET_PROD
+const CALLBACK_URL =
+  NODE_ENV === 'development' ? process.env.CB_URL_DEV : process.env.CB_URL_PROD
 
 passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+  done(null, user._id)
+})
 
 passport.deserializeUser((id, done) => {
   User.findById(id).then(user => {
-    done(null, user);
-  });
-});
+    done(null, user)
+  })
+})
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        "916830606420-8r2rpmvppildett3uar4v1rqbk6le1i2.apps.googleusercontent.com",
-      clientSecret: "qal5QZMi3B2cGTUb07nzEkJl",
-      callbackURL: "https://luch-ordering.herokuapp.com/google/callback",
-      passReqToCallback: true
+      clientID: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      callbackURL: CALLBACK_URL,
+      passReqToCallback: true,
     },
     (request, accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(currentUser => {
         if (currentUser) {
-          done(null, currentUser);
+          done(null, currentUser)
         } else {
           new User({
             username: profile.displayName,
             googleId: profile.id,
             avatar: profile._json.picture,
             roles:
-              profile.email === "kiettrankm11@gmail.com" ||
-              profile.email === "thutram54@gmail.com"
-                ? ["admin"]
-                : []
+              profile.email === 'kiettrankm11@gmail.com' ||
+              profile.email === 'thutram54@gmail.com'
+                ? ['admin']
+                : [],
           })
             .save()
             .then(newUser => {
-              done(null, newUser);
-            });
+              done(null, newUser)
+            })
         }
-      });
+      })
     }
   )
-);
+)
--- server.js
+++ server.js
@@ -1,132 +1,156 @@
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const express = require("express");
-const puppeteer = require("puppeteer");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const cookiesSession = require("cookie-session");
-const path = require("path");
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const express = require('express')
+const puppeteer = require('puppeteer')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const cookiesSession = require('cookie-session')
+const path = require('path')
+const session = require('express-session')
 
-const cors = require("cors");
+const cors = require('cors')
+dotenv.config()
 
-const config = require("./config.json");
+const config = require('./config.json')
 
-const MenuList = require("./models/menu");
-const orderRoute = require("./routes/orderDish");
+const MenuList = require('./models/menu')
+const orderRoute = require('./routes/orderDish')
 
-require("./passport-setup");
+require('./passport-setup')
 
-dotenv.config();
+const NODE_ENV = process.env.NODE_ENV
+const app = express()
+const PORT = process.env.PORT || 3000
+const db = mongoose.connection
+const REDIRECT_AUTH_URL =
+  NODE_ENV === 'development'
+    ? process.env.REDIRECT_LINK_DEV
+    : process.env.REDIRECT_LINK_PROD
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-const db = mongoose.connection;
-
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(
   cors({
-    origin: ["http://localhost:3001", "https://ordering-luch.herokuapp.com"],
-    credentials: true
+    origin: [
+      'http://localhost:3001',
+      'https://ordering-luch.herokuapp.com',
+      'http://ordering-luch.herokuapp.com',
+    ],
+    credentials: true,
   })
-);
+)
 
-const URL = "https://www.anzi.com.vn/";
+const URL = 'https://www.anzi.com.vn/'
 
+console.log(NODE_ENV)
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
-  .then(() => console.log("DB connected"));
-db.on("error", err => {
-  console.log("DB connection error:", err.message);
-});
+  .then(() => console.log('DB connected'))
+db.on('error', err => {
+  console.log('DB connection error:', err.message)
+})
 
 const getMenuList = async () => {
   try {
-    const brower = await puppeteer.launch();
-    const page = await brower.newPage();
-    await page.goto(URL);
-    const existList = await MenuList.find();
+    const brower = await puppeteer.launch()
+    const page = await brower.newPage()
+    await page.goto(URL)
+    const existList = await MenuList.find()
 
     const menuList = await page.evaluate(() => {
-      const items = document.querySelectorAll("#list_menu .item-menu");
-      let menuInfo = [];
+      const items = document.querySelectorAll('#list_menu .item-menu')
+      let menuInfo = []
       items.forEach((item = {}) => {
-        console.log(item);
-        const img = item.children[0].children[0].src;
-        const name = item.children[1].textContent.trim();
-        const price = item.children[2].children[0].textContent.trim();
-        menuInfo.push({ name, img, price });
-      });
-      return menuInfo;
-    });
+        console.log(item)
+        const img = item.children[0].children[0].src
+        const name = item.children[1].textContent.trim()
+        const price = item.children[2].children[0].textContent.trim()
+        menuInfo.push({ name, img, price })
+      })
+      return menuInfo
+    })
 
     if (existList.length === 0) {
-      MenuList.insertMany(menuList);
+      MenuList.insertMany(menuList)
     } else {
       const newList = menuList.map((item, idx) => ({
         id: existList[idx]._id,
         name: item.name,
         img: item.img,
-        price: item.price
-      }));
+        price: item.price,
+      }))
       newList.map(async (item = {}) =>
         MenuList.updateOne({ _id: item.id }, item, { upsert: false })
-      );
+      )
     }
-    return menuList;
+    return menuList
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-getMenuList();
+getMenuList()
 
-app.get("/menuList", async (request, response) => {
+app.get('/menuList', async (request, response) => {
   try {
-    const res = await MenuList.find();
-    response.send(res);
+    const res = await MenuList.find()
+    response.send(res)
   } catch (error) {
-    response.status(500).send(error);
+    response.status(500).send(error)
   }
-});
+})
 
-app.use(
-  cookiesSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [config.session],
-    secret: config.secret
-  })
-);
+// app.use(
+//   cookiesSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: [config.session],
+//     secret: config.secret,
+//     secure: {}
+//   })
+// )
 
-app.use(passport.initialize());
-app.use(passport.session());
+const sess = {
+  secret: config.secret,
+  saveUninitialized: false,
+  resave: true,
+  cookie: { secure: true },
+}
+
+if (NODE_ENV === 'development') {
+  sess.cookie.secure = false
+}
+
+app.use(session(sess))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get(
-  "/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
   })
-);
+)
 
-app.get("/google/callback", passport.authenticate("google"), (req, res) => {
-  res.redirect("https://ordering-luch.herokuapp.com");
-});
+app.get('/google/callback', passport.authenticate('google'), (req, res) => {
+  res.redirect(`${REDIRECT_AUTH_URL}`)
+})
 
-app.get("/user", (req, res) => {
+app.get('/user', (req, res) => {
   if (req.user) {
-    res.send(req.user);
+    res.send(req.user)
   } else {
-    res.send({});
+    res.send({})
   }
-});
+})
 
-app.use("/orders", orderRoute);
+app.use('/orders', orderRoute)
 
 app.listen(PORT, () => {
-  console.log("Server started on http://localhost:" + PORT);
-});
+  console.log('Server started on http://localhost:' + PORT)
+})
 
-module.exports = app;
+module.exports = app
