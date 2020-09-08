fd92502f7a446ca6fcc2b962dcd907512c766d7f
kiettran <kiet.tran@s3corp.com.vn>
2020-09-08 17:37:21 +0700

    use DB_URL back

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const URL = 'https://www.anzi.com.vn/'
 console.log(NODE_ENV)
 
 mongoose
-  .connect(process.env.PROD_DB_URL, {
+  .connect(DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
